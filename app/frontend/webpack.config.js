27c06edbca4a793a988060033acb8424e91193e9
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-03-01 18:59:41 +0100

    migrated frontend from ts to js

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -3,19 +3,33 @@ const path = require('path');
 module.exports = {
     mode: 'none',
     entry: {
-        app: path.join(__dirname, 'src', 'index.tsx')
+        app: path.join(__dirname, 'src', 'index.js')
     },
     target: 'web',
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.jsx', '.js']
     },
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: '/node_modules/'
-            }
+            { // config for es6 jsx
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+
+            { // config for images
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        }
+                    }
+                ],
+            },
         ],
     },
     output: {
