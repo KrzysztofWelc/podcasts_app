df28739e3dc48121d7b3ab498dfe32ec0fb4a282
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-03-12 16:26:16 +0100

    comments module initialized

---
+++ app/comments/models.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from app import db
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
