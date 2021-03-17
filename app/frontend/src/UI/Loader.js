0058ef597a4cd465f7f27649a86a1788b880d7fb
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-03-17 16:56:39 +0100

    Revert "backdrop component"
    This reverts commit f01a3489ed50959c08c0766761fab4656de19c8d.

--- app/frontend/src/UI/Loader.js
+++ app/frontend/src/UI/Loader.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Backdrop from "./Backdrop";
 
-export default function Loader() {
-    return (
-        <Backdrop>
+export default function Loader(){
+    return(
+        <div style={{
+            position:'fixed',
+            width: '100vw',
+            height: '100vh',
+            top: 0,
+            left: 0,
+            backgroundColor: 'white'
+        }}
+        className='d-flex align-items-center justify-content-center'
+        >
             <div className="spinner-border text-success"/>
-        </Backdrop>
+        </div>
     )
 }
---
+++ app/frontend/src/utils/debounce.js
@@ -0,0 +1,13 @@
+const debounce = (fn, delay) => {
+  let timeOutId;
+  return function(...args) {
+    if(timeOutId) {
+      clearTimeout(timeOutId);
+    }
+    timeOutId = setTimeout(() => {
+      fn(...args);
+    },delay);
+  }
+}
+
+export default debounce
