fca5a2cf9cb0d4704831288ecf95490975655ad7
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-03-17 18:33:18 +0100

    backdrop component

---
+++ app/frontend/src/UI/Backdrop.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Backdrop({clickAction, children}) {
+    return (
+        <div
+            style={{
+                position: 'fixed',
+                width: '100vw',
+                height: '100vh',
+                top: 0,
+                left: 0,
+                backgroundColor: 'white'
+            }}
+            className='d-flex align-items-center justify-content-center'
+            onClick={clickAction ? clickAction : ()=>{}}
+        >{children}</div>
+    )
+}
