133ecdd2e1e931821d3c3084ae1fa42ae419e860
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-03-24 18:51:37 +0100

    front end validation of comments

--- app/frontend/src/UI/comments/AddCommentForm.js
+++ app/frontend/src/UI/comments/AddCommentForm.js
@@ -1,21 +1,31 @@
 import React, {useState} from "react";
 
-export default function AddCommentSection({addComment}){
+export default function AddCommentSection({addComment}) {
     const [comment, setComment] = useState('')
-    const [errors, setErrors] = useState(null)
+    const [errors, setErrors] = useState([])
 
-    async function submitHandler(e){
+    async function submitHandler(e) {
         e.preventDefault()
-        await addComment(comment)
-        setComment('')
+        setErrors([])
+        if (comment !== '') {
+            await addComment(comment)
+            setComment('')
+        } else {
+            setErrors(['comment can not be empty'].concat([...errors]))
+        }
+
     }
 
-    return(
+    return (
         <form onSubmit={submitHandler}>
             <div className="form-group">
                 <label htmlFor="comment-input">Comment</label>
-                <textarea value={comment} onChange={(e)=>setComment(e.target.value)} className="form-control" id="comment-input" rows="3"/>
+                <textarea value={comment} onChange={(e) => setComment(e.target.value)} className="form-control"
+                          id="comment-input" rows="3"/>
             </div>
+            {errors.length ? errors.map(err => <div key={Math.random()}
+                                                    className="alert alert-danger">{err}</div>) : null}
+
             <button type="submit" className="btn btn-success">comment</button>
         </form>
     )
