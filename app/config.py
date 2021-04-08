5edc8937efa682be383e8ee5833e5675957b059e
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-04-08 23:52:52 +0200

    better config class

--- app/config.py
+++ app/config.py
@@ -9,6 +9,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASK_MODELS_MODULE = 'app.models'  # where all of your model classes are imported
+    PY_YAML_FIXTURES_DIR = 'app/fixtures'  # where your fixtures file(s) live
+    PY_YAML_FIXTURES_COMMAND_NAME = 'import-fixtures'  # the name of the CLI command
 
 
 class TestingConfig(BaseConfig):
