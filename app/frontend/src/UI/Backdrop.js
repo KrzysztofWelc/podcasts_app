5be716946097a8e508db04c330bec2e0871f4699
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-04-22 19:20:28 +0200

    backdrop

--- app/frontend/src/UI/Backdrop.js
+++ app/frontend/src/UI/Backdrop.js
@@ -3,16 +3,7 @@ import React from "react";
 export default function Backdrop({clickAction, children}) {
     return (
         <div
-            style={{
-                position: 'fixed',
-                width: '100vw',
-                height: '100vh',
-                top: 0,
-                left: 0,
-                backgroundColor: 'rgba(55, 173, 0, 0.48)',
-                zIndex: 50
-            }}
-            className='d-flex align-items-center justify-content-center'
+            className='flex-1 items-center justify-center z-30 bg-green-500 bg-opacity-50 fixed inset-0'
             onClick={clickAction ? clickAction : ()=>{}}
         >{children}</div>
     )
