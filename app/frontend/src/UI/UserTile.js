8707003cf67c75d277e8f9365d18053ce28078d5
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-04-22 18:45:21 +0200

    user tile styled

--- app/frontend/src/UI/UserTile.js
+++ app/frontend/src/UI/UserTile.js
@@ -5,10 +5,10 @@ export default function UserTile({user}) {
 
     return (
         <Link to={'/user/' + user.id}>
-            <div className="card" style={{minWidth: '18rem'}}>
+            <div className="border border-gray-300 rounded-md overflow-hidden w-64" style={{minWidth: '18rem'}}>
                 <img className="card-img-top" src={`/api/users/avatar/${user.profile_img}`} alt="Card image cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">{user.username}</h5>
+                <div className="p-3">
+                    <h5 className="text-4xl mb-2">{user.username}</h5>
                 </div>
             </div>
         </Link>
