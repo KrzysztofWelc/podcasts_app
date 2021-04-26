7d2adc46a3139b79b6b6c659b92b06c908ad90d5
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-04-26 16:36:54 +0200

    navbar fixed

--- app/frontend/src/UI/Navbar.js
+++ app/frontend/src/UI/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
     const history = useHistory()
     const [isMobile, setisMobile] = useState(window.matchMedia('(max-width: 640px)').matches)
     const [isNavbarActive, setIsNavbarActive] = useState(false)
-
+    
     async function handleLogout() {
         await logOut()
         history.push('/')
@@ -19,7 +19,7 @@ export default function Navbar() {
 
     const updateWidthAndHeight = () => {
         const mt = window.matchMedia('(max-width: 640px)').matches
-        if(mt != isMobile){
+        if (mt != isMobile) {
             setisMobile(mt)
         }
     };
@@ -32,40 +32,39 @@ export default function Navbar() {
     return (
         <nav className="fixed flex items-center inset-x-0 top-0 px-6 py-3 justify-between w-full bg-green-500">
             <Link to='/'><h1 className='text-3xl text-white'>Podcasts</h1></Link>
-            {isMobile && <button onClick={() => setIsNavbarActive(true)}>menu</button>}
+            {isMobile && <button onClick={() => setIsNavbarActive(true)}><img src={`${process.env.BASE_URL}assets/menu.svg`} alt="menu"/></button>}
             {isNavbarActive && <Backdrop clickAction={() => setIsNavbarActive(false)}/>}
-            <motion.div
+            <div
                 className={`items-center flex 
-                ${isMobile && 'border-box py-4 px-2 bg-gray-500 fixed inset-y-0 right-0 w-10/12 h-screen z-30 flex flex-col -right-full'}
+                    ${isMobile && `border-box py-4 px-2 bg-gray-500 fixed inset-y-0 right-0 w-10/12 h-screen z-30 flex flex-col -right-full
+                    transition-transform
+                    `}
+                    ${isNavbarActive && `-translate-y-120`}
                 `}
-                animate={{x: isNavbarActive ? '-120%' : 0}}
-                transition={{
-                    ease: "easeOut"
-                }}
             >
                 <SearchInput/>
-                <ul className="flex items-center">
+                <ul className={`flex items-center ${isMobile && 'flex-col'}`}>
                     {currentUser ? <>
                         <li className="nav-item active">
-                            <Link className="nav-link" to={`/user/${currentUser.id}`}>{currentUser.username}</Link>
+                            <Link onClick={()=>setIsNavbarActive(false)} className="nav-link" to={`/user/${currentUser.id}`}>{currentUser.username}</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/publish_podcast">Publish</Link>
+                            <Link onClick={()=>setIsNavbarActive(false)} className="nav-link" to="/publish_podcast">Publish</Link>
                         </li>
                         <li className="nav-item active">
                             <button onClick={handleLogout} className="nav-link">Log out</button>
                         </li>
                     </> : <>
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/register">Register</Link>
+                            <Link onClick={()=>setIsNavbarActive(false)} className="nav-link" to="/register">Register</Link>
                         </li>
                         <li className="nav-item active">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link onClick={()=>setIsNavbarActive(false)} className="nav-link" to="/login">Login</Link>
                         </li>
                     </>}
                 </ul>
 
-            </motion.div>
+            </div>
         </nav>
     )
 }
