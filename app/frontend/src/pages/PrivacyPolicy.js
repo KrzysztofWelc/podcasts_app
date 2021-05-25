a5a6683f6862a15d49e1e40f75495237fe1020af
krzysztofwelc <krzysiekwelc18@gmail.com>
2021-05-25 19:03:45 +0200

    footer fixed

--- app/frontend/src/pages/PrivacyPolicy.js
+++ app/frontend/src/pages/PrivacyPolicy.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 export default function PrivacyPolicy() {
     return (
-        <div className='h-screen'>
             <div className="card mt-4">
                 <h2 className='text-2xl'>Regulamin</h2>
                 <p>regulamin</p>
             </div>
-        </div>
 
     )
 }
